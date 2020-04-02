b0f9b97dc8dff04a8cb35e03da59b8967187e808
Rakibul1294 <md.rakibul.1294@gmail.com>
2020-04-03 01:10:14 +0600

    NODE-MONGO-2-Production

--- index.js
+++ index.js
@@ -3,31 +3,26 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
+// require('dotenv').config({ path: __dirname + '/.env' });
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const uri = process.env.DB_PATH;
-console.log(uri);
+// DB_PATH = mongodb+srv://dbUser:KEFGgTrz8M9bkqA8@tmcluster-wbdpu.mongodb.net/test?retryWrites=true&w=majority
 // const uri = "mongodb://localhost:27017";
+const uri = process.env.DB_PATH;
 
 let client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const users = ["Asad", "Bashir", "Abdullah", "Mamun", "Zahid", "Tamim"];
 
-
-
-// app.get('/', (req, res) => {
-//     res.send('I know how to open node ...YAY');
-// });
-
 app.get('/products', (req, res) => {
-    // client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client = new MongoClient(uri, { useNewUrlParser: true });
     client.connect(err => {
         // Database Connection
         const collection = client.db("onlineStore").collection("products");
-        collection.find({ name: 'admin' }).limit(3).toArray((err, documents) => {
+        collection.find({ name: 'Rakibul Islam' }).limit(3).toArray((err, documents) => {
             if (err) {
                 console.log(err);
                 res.status(500).send({ message: err });
@@ -36,22 +31,18 @@ app.get('/products', (req, res) => {
                 res.send(documents);
             }
         })
-        // client.close();
+        client.close();
     });
 });
 
-// app.get('/fruits/banana', (req, res) => {
-//     res.send({ fruit: 'banana', quantity: 1000, price: 10000 });
+// app.get('/products/:id', (req, res) => {
+//     console.log(req.params.id);
+//     console.log(req.query.sort);
+
+//     const id = req.params.id;
+//     const name = products[id];
+//     res.send({ id, name });
 // });
-
-app.get('/users/:id', (req, res) => {
-    console.log(req.params.id);
-    console.log(req.query.sort);
-
-    const id = req.params.id;
-    const name = users[id];
-    res.send({ id, name });
-});
 
 app.post('/addProduct', (req, res) => {
     // console.log('Data Received: ', req.body);
@@ -60,7 +51,7 @@ app.post('/addProduct', (req, res) => {
     const product = req.body;
     console.log(product);
 
-    // client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client = new MongoClient(uri, { useNewUrlParser: true });
     client.connect(err => {
         // Database Connection
         const collection = client.db("onlineStore").collection("products");
@@ -79,7 +70,7 @@ app.post('/addProduct', (req, res) => {
         })
         console.log(err);
         console.log("Database Connected ...... ");
-        // client.close();
+        client.close();
     });
 })
 
